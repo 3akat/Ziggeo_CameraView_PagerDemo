a620ceab8a83e709b08f16126d1fe726b360cae8
Alexander Bedulin <alexander.bedulin@gmail.com>
2019-10-07 16:41:06 +0300

    Code style fixed

--- app/src/main/java/com/ziggeo/pagerDemo/BlankFragmentTest1.java
+++ app/src/main/java/com/ziggeo/pagerDemo/BlankFragmentTest1.java
@@ -9,19 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.ziggeo.pagerDemo.R;
-
 
 /**
  * Created by Alexander Bedulin on 07-Oct-19.
  * Ziggeo, Inc.
  * alexb@ziggeo.com
  */
-public class BlankFragmentTest1 extends Fragment{
+public class BlankFragmentTest1 extends Fragment {
 
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_test_1, container, false);
-        }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test_1, container, false);
+    }
 }
